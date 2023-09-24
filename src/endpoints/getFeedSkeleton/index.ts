ead7a6a56be2af3f5cebc35a1a9f929ce1e7d390
Ed Costello <edc.open@gmail.com>
2023-09-24 17:15:41 +1300

    Validate request to get feed generator

--- src/endpoints/getFeedSkeleton/index.ts
+++ src/endpoints/getFeedSkeleton/index.ts
@@ -1,8 +1,36 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
-export const handler = async (
+import middy from '@middy/core';
+import httpHeaderNormalizer from '@middy/http-header-normalizer';
+import httpErrors from 'http-errors';
+import { verifyJwt } from '@atproto/xrpc-server';
+import { DidResolver, MemoryCache } from '@atproto/did-resolver';
+
+const didCache = new MemoryCache();
+const didResolver = new DidResolver(
+  { plcUrl: 'https://plc.directory' },
+  didCache
+);
+
+export const rawHandler = async (
   event: APIGatewayProxyEventV2
 ): Promise<APIGatewayProxyResultV2> => {
   console.log(JSON.stringify(event, undefined, 2));
+
+  const { authorization = '' } = event.headers;
+  if (!authorization.startsWith('Bearer ')) {
+    throw new httpErrors.Unauthorized();
+  }
+  const jwt = authorization.replace('Bearer ', '').trim();
+  const requesterDid = await verifyJwt(
+    jwt,
+    `did:web:${process.env.PUBLIC_HOSTNAME}`,
+    async (did: string) => {
+      return didResolver.resolveAtprotoKey(did);
+    }
+  );
+
+  console.log({ requesterDid });
+
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -20,3 +48,5 @@ export const handler = async (
     },
   };
 };
+
+export const handler = middy(rawHandler).use(httpHeaderNormalizer());
