059a14c4b6522fcf2da1e64c23e313d800fc8e47
Ed Costello <edc.open@gmail.com>
2023-09-24 20:32:36 +1300

    unmarshal dynamo event

--- src/endpoints/syncSubscriberFollowing/index.ts
+++ src/endpoints/syncSubscriberFollowing/index.ts
@@ -1,8 +1,12 @@
-import { DynamoDBStreamEvent } from 'aws-lambda';
 import middy from '@middy/core';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
 
-export const rawHandler = async (event: DynamoDBStreamEvent): Promise<void> => {
+export const rawHandler = async (event: unknown): Promise<void> => {
   console.log(JSON.stringify(event, undefined, 2));
 };
 
-export const handler = middy(rawHandler);
+export const handler = middy(rawHandler).before((request) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  request.event = unmarshall(request.event.Records[0].dynamodb?.NewImage);
+});
