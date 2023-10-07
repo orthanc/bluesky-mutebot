e115b43889cee326d65c92ac8f0100e415061984
Ed Costello <edc.open@gmail.com>
2023-10-07 15:43:56 +1300

    Define posts table

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,28 @@ export type FollowingEntry = {
   handle: string;
 };
 export type FollowingSet = Record<string, Omit<FollowingEntry, 'did'>>;
+
+export type PostEntry = {
+  uri: string;
+  createdAt: string;
+  author: string;
+  resolvedStatus: 'UNRESOLVED' | 'RESOLVED';
+  expiresAt?: number;
+  isReply?: true;
+  replyRootUri?: string;
+  replyRootAuthorDid?: string;
+  replyParentUri?: string;
+  replyRootParentDid?: string;
+  startsWithMention?: true;
+  mentionedDids: Array<string>;
+  textEntries: Array<string>;
+};
+
+export type PostRecord = {
+  uri: string;
+  createdAt: string;
+  author: string;
+} & (
+  | ({ type: 'post' } & PostEntry)
+  | { type: 'repost'; repostedPostUri: string; post?: PostEntry }
+);
