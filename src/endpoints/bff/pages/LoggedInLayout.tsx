f69d87f95c168ebed5050ce53aae5968dfbd6ed6
Ed Costello <edc.open@gmail.com>
2023-11-24 14:38:07 +1300

    logout top and bottom

--- src/endpoints/bff/pages/LoggedInLayout.tsx
+++ src/endpoints/bff/pages/LoggedInLayout.tsx
@@ -5,6 +5,11 @@ export const LoggedInLayout: preact.FunctionComponent = ({ children }) => (
     <form hx-post="/logout" hx-target="body" hx-trigger="submit throttle:1s">
       <input type="submit" value="Logout" />
     </form>
+    <hr />
     <Content>{children}</Content>
+    <hr />
+    <form hx-post="/logout" hx-target="body" hx-trigger="submit throttle:1s">
+      <input type="submit" value="Logout" />
+    </form>
   </>
 );
