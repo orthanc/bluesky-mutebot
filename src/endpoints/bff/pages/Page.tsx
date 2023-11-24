a1a69410437226c3f69c094142c70382cb34338e
Ed Costello <edc.open@gmail.com>
2023-11-24 20:52:07 +1300

    initial tailwind setup

--- src/endpoints/bff/pages/Page.tsx
+++ src/endpoints/bff/pages/Page.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 const STATIC_CONTENT_BASE = process.env.IS_OFFLINE
-  ? `https://${process.env.WEB_DOMAIN_NAME}`
-  : '';
+  ? `http://localhost:8080`
+  : '/static';
 
 function gloabalEventHandler() {
   window.addEventListener('DOMContentLoaded', () => {
@@ -37,33 +37,39 @@ export const Page: preact.FunctionComponent<PageProps> = ({
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>Mutebot Control Panel</title>
       <link
+        href={`${STATIC_CONTENT_BASE}/${
+          process.env.IS_OFFLINE
+            ? `mutebot.dev.css`
+            : `${process.env.MUTEBOT_CSS_NAME}`
+        }`}
+        rel="stylesheet"
+      />
+      <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href={`${STATIC_CONTENT_BASE}/static/favicon-16x16-cf9f061f2cae78c166301a54e2fbd87cdb243273dd2b38c58c23987fde4159ad.png`}
+        href={`${STATIC_CONTENT_BASE}/favicon-16x16-cf9f061f2cae78c166301a54e2fbd87cdb243273dd2b38c58c23987fde4159ad.png`}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href={`${STATIC_CONTENT_BASE}/static/favicon-32x32-2f075c48175c1abb1338119688f90b3525d1e9802fd8a4ca604868faa1112fff.png`}
+        href={`${STATIC_CONTENT_BASE}/favicon-32x32-2f075c48175c1abb1338119688f90b3525d1e9802fd8a4ca604868faa1112fff.png`}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="96x96"
-        href={`${STATIC_CONTENT_BASE}/static/favicon-96x96-8972dc60894a5e7f7395075e23a0d07c4aa37b622c3fc950b1af81d17ab69e35.png`}
+        href={`${STATIC_CONTENT_BASE}/favicon-96x96-8972dc60894a5e7f7395075e23a0d07c4aa37b622c3fc950b1af81d17ab69e35.png`}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="180x180"
-        href={`${STATIC_CONTENT_BASE}/static/favicon-180x180-9d5072e30a9e91304cb40a8d08541c2db1e2a0d8dbadf76f68b5d2f8821a342d.png`}
+        href={`${STATIC_CONTENT_BASE}/favicon-180x180-9d5072e30a9e91304cb40a8d08541c2db1e2a0d8dbadf76f68b5d2f8821a342d.png`}
       />
-      <script src={`${STATIC_CONTENT_BASE}/static/htmx-1.9.9.min.js`}></script>
-      <script
-        src={`${STATIC_CONTENT_BASE}/static/htmx-ext-ws-1.9.9.js`}
-      ></script>
+      <script src={`${STATIC_CONTENT_BASE}/htmx-1.9.9.min.js`}></script>
+      <script src={`${STATIC_CONTENT_BASE}/htmx-ext-ws-1.9.9.js`}></script>
       {csrfToken == null ? null : (
         <script
           dangerouslySetInnerHTML={{
